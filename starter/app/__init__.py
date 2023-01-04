8dd2c771cd0deb94982a7ae7f1473cc03a660de5
Alex Castillo <agcast01@gmail.com>
2023-01-04 13:46:48 -0500

    setup first route

---
+++ starter/app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from .config import Config
+from .tweets import tweets
+from random import choice
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def home():
+    tweet = choice(tweets)
+    return render_template('index.html', tweet=tweet)
