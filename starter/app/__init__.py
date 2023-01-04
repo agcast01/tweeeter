aa3b0b98a6a426db3a42c35414263ceef8221b0a
drewduong <105185556+drewduong@users.noreply.github.com>
2023-01-04 14:35:16 -0800

    Finished with phase 3 and first problem in bonus

--- starter/app/__init__.py
+++ starter/app/__init__.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from .config import Config
 from .tweets import tweets
 from random import choice
 from .forms.form import CreateTweetForm
+import datetime
 
 app = Flask(__name__)
 
@@ -17,10 +18,29 @@ def home():
 
 @app.route('/feed')
 def feed():
-    return render_template('feed.html', tweets=tweets)
+    def sorter(tweet):
+        a_list = tweet['date'].split('/')
+        return datetime.date(int(a_list[2]) + 2000, int(a_list[0]), int(a_list[1]))
+    sorted_by_date = sorted(tweets, key=sorter, reverse=True)
+    return render_template('feed.html', tweets=sorted_by_date)
 
 
 @app.route('/new', methods=['GET', 'POST'])
 def newTweet():
     form = CreateTweetForm()
+
+    if form.validate_on_submit():
+        new_tweet = {
+            'id': len(tweets),
+            'author': form.data['author'],
+            'tweet': form.data['tweet'],
+            'date': datetime.date.today(),
+            'likes': 0
+        }
+        tweets.append(new_tweet)
+        return redirect('/feed')
+
+    if form.errors:
+        return form.errors
+
     return render_template('new_tweet.html', form=form)
